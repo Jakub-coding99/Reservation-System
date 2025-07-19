e35aa973e97625fcc67e948e81432631ac74510a
Jakub Večeřa <kuba.vecera@email.cz>
2025-07-20 00:49:25 +0200

    created change_reservation and delete_client. table created in html.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request, jsonify
+from flask import Flask, render_template, redirect, url_for, request
 from db_model import db, Clients
 import os
 from dotenv import load_dotenv, find_dotenv
@@ -20,7 +20,8 @@ with app.app_context():
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    clients = get_reservation()
+    return render_template("index.html", clients = clients, )
 
 @app.route("/reservation", methods = ["GET", "POST"])
 def save_reservation():
@@ -30,19 +31,39 @@ def save_reservation():
         "time" : request.form.get("time"),
         "phone" : request.form.get("phone")
     }
-    print(reservation)
-    # format_date = "%Y-%m-%d"
-    # datetime_date = datetime.datetime.strptime(reservation["date"], format_date)
+    format_date = "%Y-%m-%d"
+    datetime_date = datetime.datetime.strptime(reservation["date"], format_date)
     
-    # x = reservation["time"].split(":")
-    # datetime_time = datetime.time(hour=int(x[0]), minute=int(x[1]))
+    x = reservation["time"].split(":")
+    datetime_time = datetime.time(hour=int(x[0]), minute=int(x[1]))
     
-    # new_client = Clients(name = reservation["name"], date = datetime_date, time = datetime_time, phone = int(reservation["phone"]))
-    # db.session.add(new_client)
-    # db.session.commit()
+    new_client = Clients(name = reservation["name"], date = datetime_date, time = datetime_time, phone = int(reservation["phone"]))
+    db.session.add(new_client)
+    db.session.commit()
     
     return redirect(url_for("home"))
 
+def get_reservation():
+    with app.app_context():
+        clients = db.session.query(Clients).all()
+        return clients
+
+@app.route("/delete/<int:id>")
+def delete_client(id):
+    to_delete = db.session.query(Clients).get(id)
+    db.session.delete(to_delete)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+@app.route("/change/<int:id>")
+def change_reservation(id):
+    to_change = db.session.query(Clients).get(id)
+    print(to_change.name)
+
+    
+    
+    
+    return redirect(url_for("home"))
 
 
 def send_message():
